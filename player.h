56570281d095e4e4e8d82875b9455be80bcfd637
qianyunfan <78582603+qianyunfan@users.noreply.github.com>
2022-08-05 17:52:24 +0800

    Update player.h

--- player.h
+++ player.h
@@ -4,7 +4,7 @@ class Status
 {
 public:
 	int blood = 0;
-	int Qi = 0;
+	static int Qi;
 	int attack = 0;
 	int defend = 0;
 };
@@ -15,16 +15,29 @@ public:
 		id_(id)
 	{
 		init();
-
 	}
 public:
 	int id_;
 	Status status;
 	Skill* skill;
 	void putSkill() {
-		skill->chooseSkill(Skills::CA);
+		auto tuple = skill->chooseSkill(Skills::CA);
+		int type = std::get<0>(tuple);
+		int value = std::get<1>(tuple);
+		switch (type)
+		{
+		case 0: status.Qi += value; break;
+		case 1: status.defend += value; break;
+		case 2: status.attack += value; break;
+		default: {
+			exception e;
+			throw(e);
+			break;
+		}
+		}
 	};
-	void receiveSkill() {
+	void receiveSkill()
+	{
 	};
 public:
 	void setdefend(int def)
@@ -36,6 +49,12 @@ public:
 		status.attack = att;
 	};
 private:
+	//每回合结束后重置一些属性
+	void resetAttribute()
+	{
+		status.attack = 0;
+		status.defend = 0;
+	}
 	void init()
 	{
 		status.blood = 1;
